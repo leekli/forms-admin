e22acb8251a44ad8b0d7c96a23af9ad36d5864aa
Thomas Iles <tomiles@gmail.com>
2026-06-15 13:49:11 +0100

    Move route label into route input object
    Previously, the route label was created in the build service and stored
    in the routes input object. This meant that labels were not included
    when a validation error was displayed on the edit routes page.
    
    This commit moves the label logic into a method on the route input.
    
    It's removed from the build service and it's tests.
    
    It also adapts the label code to include the option index for selection
    pages with none of the above.

--- app/input_objects/forms/route_input.rb
+++ app/input_objects/forms/route_input.rb
@@ -9,7 +9,7 @@ class Forms::RouteInput < BaseInput
   attribute :goto
   attribute :answer_value
 
-  attr_accessor :page, :goto_page, :goto_options, :label
+  attr_accessor :page, :goto_page, :goto_options
 
   validate :route_is_not_backwards
 
@@ -31,6 +31,15 @@ class Forms::RouteInput < BaseInput
     end
   end
 
+  def label
+    if Forms::RoutesInput.route_with_selection_options?(page)
+      answer_value_label = answer_value == Condition::NONE_OF_THE_ABOVE ? I18n.t("page_conditions.none_of_the_above") : answer_value
+      return { text: "If option #{option_index} (#{answer_value_label}), go to:" }
+    end
+
+    { text: "After question #{page.position}, go to:" }
+  end
+
 private
 
   def route_is_not_backwards
@@ -38,7 +47,7 @@ private
 
     return if goto_page.position > page.position
 
-    errors.add(:goto, error_message_for_backwards_route)
+    errors.add(:goto, :backwards_route, message: error_message_for_backwards_route)
   end
 
   def skippable_for_backwards_validation?
@@ -47,10 +56,17 @@ private
 
   def error_message_for_backwards_route
     if Forms::RoutesInput.route_with_selection_options?(page)
-      option_index = page.answer_settings.selection_options.find_index { |option| option["value"] == answer_value }
-      I18n.t("errors.routes.cannot_route_backwards_from_selection_page", question_number: page.position, option_number: option_index + 1)
+      I18n.t("errors.routes.cannot_route_backwards_from_selection_page", question_number: page.position, option_number: option_index)
     else
       I18n.t("errors.routes.cannot_route_backwards_from_generic_page", question_number: page.position)
+    end
+  end
+
+  def option_index
+    if answer_value == Condition::NONE_OF_THE_ABOVE
+      page.answer_settings.selection_options.length + 1
+    else
+      page.answer_settings.selection_options.find_index { |option| option["value"] == answer_value } + 1
     end
   end
 end
