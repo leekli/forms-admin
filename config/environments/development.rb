965385b633da5c3152e227697910ba22cd57e373
李 li [lee] <li.kirkham@pm.me>
2026-07-06 15:02:21 +0000

    excludes CSRF checks if on codespaces dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,6 +65,10 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  if ENV["CODESPACES"] == "true"
+    config.action_controller.forgery_protection_origin_check = false
+  end
+
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
 
